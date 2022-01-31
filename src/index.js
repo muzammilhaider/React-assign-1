b5ce886334d328971ec3be231a0f0c0b0387512a
Muzammil Haider <muzammilhaider110@gmail.com>
2022-01-31 09:43:18 +0500

    React assign 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <h1>Name: Muzammil Haider</h1>
+    <h4>Email: muzammilhaider110@gmail.com</h4>
+    <h4>Education: BSCS</h4>
+  </>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
